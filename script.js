4e3e73c634ca47e5169aa67ac5eaac500a87e730
William Myers <williamrmyers@gmail.com>
2016-12-06 23:08:40 -0800

    Update script.js
    Added Analytics

---
+++ script.js
@@ -0,0 +1,28 @@
+// Analytics
+// Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88365479-1', 'auto');
+  ga('send', 'pageview');
+// Google Analytics End
+
+// Kissmetrics
+
+// End Kissmetrics
+var _kmq = _kmq || [];
+var _kmk = _kmk || '454b9d10c76d9522faf401b657523b8f0c4707f3';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
+
+// End Analytics
